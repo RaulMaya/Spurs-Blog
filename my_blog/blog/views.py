f16ba83be638f0734e3744e807b1fbbeb75f20bf
Raul <raulmayas20@gmail.com>
2022-05-26 15:21:35 -0500

    Index Html and Base with styling

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def starting_page(request):
-    pass
+    return render(request, "blog/index.html")
 
 def posts(request):
     pass
 
-def individual_post(request)
+def individual_post(request):
+    pass
