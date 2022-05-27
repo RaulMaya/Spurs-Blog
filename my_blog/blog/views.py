46c4023427bc384f524ce8ea7969217fccb6f737
Raul <raulmayas20@gmail.com>
2022-05-27 09:34:48 -0500

    Adding data for the blog

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,7 +1,28 @@
 import re
 from django.shortcuts import render
+from datetime import date
 
 # Create your views here.
+posts = [
+    {
+        "slug":"real_madrid_decision",
+        "image":"real_madrid_options.jpg",
+        "author":"Raul Maya",
+        "date":date(2022,5,27),
+        "title":"Real Madrid Options To Mbappe News",
+        "excerpt":"Nkunku, Salah, Botman, Mane, Gnabry",
+        "content": """
+
+        Even though in my opinion Real Madrid doesn't need to reinforce the attack, and I feel that Mbappe situation was all about
+        bringing in a powerful and important name, Florentino must be looking for great names to bring in to Real Madrid.
+        
+        Even though in my opinion Real Madrid doesn't need to reinforce the attack, and I feel that Mbappe situation was all about
+        bringing in a powerful and important name, Florentino must be looking for great names to bring in to Real Madrid.
+        
+        Even though in my opinion Real Madrid doesn't need to reinforce the attack, and I feel that Mbappe situation was all about
+        bringing in a powerful and important name, Florentino must be looking for great names to bring in to Real Madrid.""",
+    }
+]
 
 def starting_page(request):
     return render(request, "blog/index.html")
@@ -9,5 +30,5 @@ def starting_page(request):
 def posts(request):
     return render(request, "blog/posts.html")
 
-def individual_post(request):
-    pass
+def individual_post(request, slug):
+    return render(request, "blog/individual_post.html")
