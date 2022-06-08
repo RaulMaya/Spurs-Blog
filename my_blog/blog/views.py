9339e8b675611b3d6fdf8e0bd052f25c0b86f09e
Raul <raulmayas20@gmail.com>
2022-06-08 17:24:34 -0500

    Creating models and establishing relationships

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -120,20 +120,67 @@ def get_date(post):
     return post['date']
 
 def starting_page(request):
+    list_of_dicts = []
     posts = Post.objects.all().order_by("date")
+    print(len(posts))
     print(posts[0].title, posts[0].author, posts[0].date)
+    for post in posts:
+        dictionary = {
+          "slug": post.slug,
+          "image": post.image_name,
+          "author": post.author,
+          "date": post.date,
+          "title": post.title,
+          "excerpt": post.excerpt,
+          "content": post.content,
+          "tag": post.tag
+          }
+        list_of_dicts.append(dictionary)
+
     sorted_posts = sorted(written_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = list_of_dicts[-3:]
     return render(request, "blog/index.html", {"posts":latest_posts})
 
 def posts(request):
+    list_of_dicts = []
+    posts = Post.objects.all().order_by("date")
+    print(len(posts))
+    print(posts[0].title, posts[0].author, posts[0].date)
+    for post in posts:
+        dictionary = {
+          "slug": post.slug,
+          "image": post.image_name,
+          "author": post.author,
+          "date": post.date,
+          "title": post.title,
+          "excerpt": post.excerpt,
+          "content": post.content,
+          "tag": post.tag
+          }
+        list_of_dicts.append(dictionary)
     sorted_posts = sorted(written_posts, key=get_date)
     return render(request, "blog/posts.html", {
-        "all_posts":sorted_posts
+        "all_posts":list_of_dicts
     })
 
 def individual_post(request, slug):
-    selected_post = next(post for post in written_posts if post['slug'] == slug)
+    list_of_dicts = []
+    posts = Post.objects.all().order_by("date")
+    print(len(posts))
+    print(posts[0].title, posts[0].author, posts[0].date)
+    for post in posts:
+        dictionary = {
+          "slug": post.slug,
+          "image": post.image_name,
+          "author": post.author,
+          "date": post.date,
+          "title": post.title,
+          "excerpt": post.excerpt,
+          "content": post.content,
+          "tag": post.tag
+          }
+        list_of_dicts.append(dictionary)
+    selected_post = next(post for post in list_of_dicts if post['slug'] == slug)
     return render(request, "blog/individual_post.html", {
         "post": selected_post
     })
