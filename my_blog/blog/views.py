5930425ceaf63e696b61cac64608035421f7bb31
Raul <raulmayas20@gmail.com>
2022-06-10 14:03:32 -0500

    Working with the database and filling the posts

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,8 +1,9 @@
 from turtle import write_docstringdict
+from xml.dom.minidom import Identified
 from django.shortcuts import render
 from datetime import date
 from django.db.models import Avg, Max, Min
-from .models  import Post, Tag, Author
+from .models  import Post
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -11,67 +12,26 @@ def get_date(post):
     return post['date']
 
 def starting_page(request):
-    list_of_dicts = []
-    posts = Post.objects.all().order_by("date")
-    print(len(posts))
-    print(posts[0].title, posts[0].author, posts[0].date)
-    for post in posts:
-        dictionary = {
-          "slug": post.slug,
-          "image": post.image_name,
-          "author": post.author,
-          "date": post.date,
-          "title": post.title,
-          "excerpt": post.excerpt,
-          "content": post.content,
-          "tag": post.tag
-          }
-        list_of_dicts.append(dictionary)
+    latest_posts = Post.objects.all().order_by("-date")[:3]
 
-    sorted_posts = sorted(written_posts, key=get_date)
-    latest_posts = list_of_dicts[-3:]
+    # sorted_posts = sorted(written_posts, key=get_date)
+    # latest_posts = list_of_dicts[-3:]
+
     return render(request, "blog/index.html", {"posts":latest_posts})
 
 def posts(request):
     list_of_dicts = []
-    posts = Post.objects.all().order_by("date")
-    print(len(posts))
-    print(posts[0].title, posts[0].author, posts[0].date)
-    for post in posts:
-        dictionary = {
-          "slug": post.slug,
-          "image": post.image_name,
-          "author": post.author,
-          "date": post.date,
-          "title": post.title,
-          "excerpt": post.excerpt,
-          "content": post.content,
-          "tag": post.tag
-          }
-        list_of_dicts.append(dictionary)
-    sorted_posts = sorted(written_posts, key=get_date)
+    posts = Post.objects.all().order_by("-date")
+
+    # sorted_posts = sorted(written_posts, key=get_date)
     return render(request, "blog/posts.html", {
-        "all_posts":list_of_dicts
+        "all_posts":posts
     })
 
 def individual_post(request, slug):
-    list_of_dicts = []
-    posts = Post.objects.all().order_by("date")
-    print(len(posts))
-    print(posts[0].title, posts[0].author, posts[0].date)
-    for post in posts:
-        dictionary = {
-          "slug": post.slug,
-          "image": post.image_name,
-          "author": post.author,
-          "date": post.date,
-          "title": post.title,
-          "excerpt": post.excerpt,
-          "content": post.content,
-          "tag": post.tag
-          }
-        list_of_dicts.append(dictionary)
-    selected_post = next(post for post in list_of_dicts if post['slug'] == slug)
+    selected_post = get_object_or_404(Post, slug=slug)
+    # posts = Post.objects.all().order_by("date")
+    # selected_post = next(post for post in posts if post.slug == slug)
     return render(request, "blog/individual_post.html", {
-        "post": selected_post
+        "post": selected_post, "tags":selected_post.tag.all()
     })
