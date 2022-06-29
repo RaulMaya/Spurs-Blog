ce105fcee526b912eb1991c18609e94276320da0
Raul <raulmayas20@gmail.com>
2022-06-29 12:23:15 -0500

    Adding the Class Views and Creating Comments Form

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('posts', views.PostsView.as_view()),
-    path('posts/<int:pk>', views.DetailPostView.as_view()), #/posts/real-madrids-wild-cards
+    path('', views.IndexView.as_view(), name = 'starting_page'),
+    path('posts', views.PostsView.as_view(), name = 'posts'),
+    path('posts/<slug:slug>',views.DetailPostView.as_view(), name = 'individual_post'), #/posts/real-madrids-wild-cards
 ]
