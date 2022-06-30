abcc957c1299265f255a7424cae8debc871e9cfb
Raul <raulmayas20@gmail.com>
2022-06-30 16:23:01 -0500

    Saving favorites and read later books

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,4 +1,5 @@
 from datetime import date
+from multiprocessing import context
 from django.db.models import Avg, Max, Min
 from .models  import Post
 from django.urls import reverse
@@ -92,3 +93,36 @@ class DetailPostView(View):
 #    return render(request, "blog/individual_post.html", {
 #        "post": selected_post, "tags":selected_post.tag.all()
 #    })
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        
+        return render(request, "blog/stored-posts.html", context)
+
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+        
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+        
+
+
+        return HttpResponseRedirect("/")
