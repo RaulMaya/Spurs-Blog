054b4c18083dd5037dfaef55a7f3f8a143034383
Raul <raulmayas20@gmail.com>
2022-06-30 16:23:01 -0500

    Saving favorites and read later books

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name = 'starting_page'),
     path('posts', views.PostsView.as_view(), name = 'posts'),
     path('posts/<slug:slug>',views.DetailPostView.as_view(), name = 'individual_post'), #/posts/real-madrids-wild-cards
+    path('read-later', views.ReadLaterView.as_view(), name ='read-later')
 ]
