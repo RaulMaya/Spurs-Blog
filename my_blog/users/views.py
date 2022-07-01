910441eec06969e9ed4092fb0bb1bcd8f73d1fcb
Raul <raulmayas20@gmail.com>
2022-07-01 11:49:57 -0500

    User Authentication Loaded

--- my_blog/users/views.py
+++ my_blog/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {'form':form}
+        
+        return render(request, "registration/register.html", context)
+
+    def post(self, request):
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("blog:starting_page")
