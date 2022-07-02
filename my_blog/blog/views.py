fe17b8a3c2a66d5fcdc09def142ba7842e6a1473
Raul <raulmayas20@gmail.com>
2022-07-02 14:02:36 -0500

    User Authentication

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -140,3 +140,38 @@ class ReadLaterView(View):
 
 
         return HttpResponseRedirect("/")
+
+class FavoritesView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        
+        return render(request, "blog/favorites.html", context)
+
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+        
+        if post_id in stored_posts:
+            stored_posts.remove(post_id)
+            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+        
+
+
+        return HttpResponseRedirect("/")
